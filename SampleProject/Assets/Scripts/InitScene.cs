5f77e26bce4dab3922c3b8e136b0f82dcc573fdc
CHOI CHANG HYUK <forber4180@ngelgames.com>
2022-02-28 11:35:32 +0900

    에셋매니저에 에셋 등록 부분 롤백

--- SampleProject/Assets/Scripts/InitScene.cs
+++ SampleProject/Assets/Scripts/InitScene.cs
@@ -106,12 +106,6 @@ public class InitScene : MonoBehaviour
 
                 //crc 일치하지 않을 때 (null) 새로 다운로드
                 needDownload = bundleLoadRequest.assetBundle == null;
-
-                //새로 다운로드 할 필요가 없을 경우 에셋 매니저 클래스에 에셋번들 추가
-                if (needDownload == false)
-                {
-                    AssetManager.Instance.AddAssetBundle(bundleLoadRequest.assetBundle);
-                }
             }
 
             if (needDownload)
@@ -122,15 +116,6 @@ public class InitScene : MonoBehaviour
                     localSaveUrl = localAssetSaveUrl + bundleInfo.bundleName,
                     onComplete = x =>
                     {
-                        //다운로드 완료 후 에셋번들 로드
-                        var bundleLoadRequest = AssetBundle.LoadFromFileAsync(localAssetSaveUrl + bundleInfo.bundleName, bundleInfo.crc);
-
-                        //정상적으로 로드 됬다면 에셋 매니저 클래스에 에셋번들 추가
-                        if (bundleLoadRequest.assetBundle != null)
-                        {
-                            AssetManager.Instance.AddAssetBundle(bundleLoadRequest.assetBundle);
-                        }
-
                         if (progressBar) progressBar.Init(downloadStream.currentJobIndex, (float)downloadStream.maxJobSize, true);
 
                         Debug.Log("Download Complete : " + bundleInfo.bundleName);
